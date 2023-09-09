6966ac82f712c1f05fe40e6e5e5d8b08b3edcfa8
iuna Leon <yuna.ads@hotmail.com>
2023-09-08 21:41:04 -0300

    mobile version

--- src/components/organisms/Hero/Hero.jsx
+++ src/components/organisms/Hero/Hero.jsx
@@ -13,6 +13,26 @@ export const Hero = () => {
               </div>
               <img className={Style.Hero__lines} src="/images/lines.png" />
               <img className={Style.Hero__Oval} src="/images/Oval.png" />
+              <div className={Style.Hero__introduction}>
+                  <h1 className={Style.Hero__introduction__title}>
+                      Nice to meet you! I’m{" "}
+                      <u className={Style.Hero__introduction__title__underline}>
+                          Iuna de Leon
+                      </u>
+                      .
+                  </h1>
+                  <p className={Style.Hero__introduction__paragraph}>
+                      Based in the BR, I’m a front-end developer passionate
+                      about building accessible web apps that users love.
+                  </p>
+                  <a className={Style.Hero__introduction__anchor}>contact me</a>
+              </div>
+              <hr
+                  size="1"
+                  width="90%"
+                  align="center"
+                  color="rgba(255, 255, 255, 1)"
+              />
           </div>
       </>
   );
