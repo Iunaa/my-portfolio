4ce1fe9d58cbcafae80b5fb2bedef22b2b150eda
iuna Leon <yuna.ads@hotmail.com>
2023-09-10 20:03:08 -0300

    Desktop version2.0

--- src/components/organisms/Hero/Hero.jsx
+++ src/components/organisms/Hero/Hero.jsx
@@ -6,17 +6,18 @@ export const Hero = () => {
       <>
           <div className={Style.Hero}>
               <div className={Style.Hero__container}>
-                  <img
+                  {/* <img
                       className={Style.Hero__container__img}
                       src="/images/photo.png"
-                  />
+                  /> */}
               </div>
-              <img className={Style.Hero__lines} src="/images/lines.png" />
-              <img className={Style.Hero__Oval} src="/images/Oval.png" />
+              <img className={Style.Hero__lines} src="/images/Group26.png" />
+              <img className={Style.Hero__Oval} src="/images/circle.svg" />
               <div className={Style.Hero__introduction}>
                   <h1 className={Style.Hero__introduction__title}>
                       Nice to meet you! I’m{" "}
                       <u className={Style.Hero__introduction__title__underline}>
+                        
                           Iuna de Leon
                       </u>
                       .
@@ -27,7 +28,7 @@ export const Hero = () => {
                   </p>
                   <a className={Style.Hero__introduction__anchor}>contact me</a>
               </div>
-              <hr size="1" width="90%" align="center" color='rgba(255, 255, 255, 1)'/>
+              <hr size="1" width="90%"  align="center"/>
           </div>
       </>
   );
